ade3bcf11b6789c16036aa07411e974a32e7faba
alice <107301994+alicelilium@users.noreply.github.com>
2023-04-16 17:22:05 -0700

    Update DeckofCards.java
    Update formatting

--- Card/cardpkg/DeckofCards.java
+++ Card/cardpkg/DeckofCards.java
@@ -17,31 +17,31 @@ public class DeckofCards {
         deck = new Card[52];
         int i = 0;
 		
-		//Goes through each suit, adding one of each face value until it gets to 13 then next suit
+	//Goes through each suit, adding one of each face value until it gets to 13 then next suit
         for (int s = 0; s < 4; s++) {
             for (int f = 0; f < 13; f++) {
-                deck[i++] = new Card(f + 1, s + 1); //Stores the card, index goes up each time
+                deck[i++] = new Card(f + 1, s + 1);	//Stores the card, index goes up each time
             }
         }
     }
 	
-	//Allows user to deal a card
+    //Allows user to deal a card
     public String deal() {
         String dealtCard;
         if (currentCard == 52)
-            return "Deck is empty"; //Deck is empty once card reaches max, must be shuffled.
+            return "Deck is empty";	//Deck is empty once card reaches max, must be shuffled.
         else {
-			//If deck not empty, deal card and go to next card for next deal
+	    //If deck not empty, deal card and go to next card for next deal
             dealtCard = "The " + deck[currentCard] + " has been dealt.";
             currentCard++;
             return dealtCard;
         }
     }
 
-	//Allows user to shuffle the deck
+    //Allows user to shuffle the deck
     public String shuffle() {
 		
-		//Randomly "shuffle" 100 times - switch the places of two cards picked at random
+	//Randomly "shuffle" 100 times - switch the places of two cards picked at random
         for (int i = 0; i < 100; i++) {
             int index = rand.nextInt(52);
             int index2 = rand.nextInt(52);
@@ -49,21 +49,21 @@ public class DeckofCards {
             deck[index2] = deck[index];
             deck[index] = temp;
         }
-        currentCard = 0;        //Assume when deck is shuffled, all dealt cards are reclaimed
+        currentCard = 0;	//Assume when deck is shuffled, all dealt cards are reclaimed
         return "The deck has been shuffled.";
     }
 
-	//Returns the remaining amount of cards in the deck
+    //Returns the remaining amount of cards in the deck
     public int cardsLeft() {
         return deck.length - currentCard;
     }
 
-	//Prints the whole deck back to user
+    //Prints the whole deck back to user
     public String toString() {
         String result = "";
         for (int i = 0; i < deck.length ; i++) {
                 if ((i+1)%13 == 0 && i != 0)
-                    result += deck[i] + ", \n"; //New line after each suit
+                    result += deck[i] + ", \n";		//New line after each suit
                 else
                     result += deck[i] + ", ";
             }
